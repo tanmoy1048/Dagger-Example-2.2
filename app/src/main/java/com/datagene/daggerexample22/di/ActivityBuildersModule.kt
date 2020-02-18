93b84bdaef8ec704f0793a12a46c3f491fcd8b9b
Rafi <rsadat@datagene.com.au>
2020-02-18 21:49:42 +1100

    no message

--- app/src/main/java/com/datagene/daggerexample22/di/ActivityBuildersModule.kt
+++ app/src/main/java/com/datagene/daggerexample22/di/ActivityBuildersModule.kt
@@ -4,7 +4,6 @@ import com.datagene.daggerexample22.di.another.AnotherModule
 import com.datagene.daggerexample22.di.another.AnotherViewModelsModule
 import com.datagene.daggerexample22.di.main.MainModule
 import com.datagene.daggerexample22.di.main.MainViewModelsModule
-import com.datagene.daggerexample22.di.main.ProfileFragmentBuildersModule
 import com.datagene.daggerexample22.ui.AnotherActivity
 import com.datagene.daggerexample22.ui.MainActivity
 import dagger.Module
@@ -12,7 +11,7 @@ import dagger.android.ContributesAndroidInjector
 
 @Module
 abstract class ActivityBuildersModule {
-    @ContributesAndroidInjector(modules = [ProfileFragmentBuildersModule::class, MainViewModelsModule::class, MainModule::class])
+    @ContributesAndroidInjector(modules = [MainViewModelsModule::class, MainModule::class])
     abstract fun contributeMainActivity(): MainActivity
 
     @ContributesAndroidInjector(modules = [AnotherViewModelsModule::class, AnotherModule::class])
--- app/src/main/java/com/datagene/daggerexample22/di/main/MainViewModelsModule.kt
+++ app/src/main/java/com/datagene/daggerexample22/di/main/MainViewModelsModule.kt
@@ -3,7 +3,6 @@ package com.datagene.daggerexample22.di.main
 import androidx.lifecycle.ViewModel
 import com.datagene.daggerexample22.di.ViewModelKey
 import com.datagene.daggerexample22.ui.MainActivityViewModel
-import com.datagene.daggerexample22.ui.fragment.ProfileFragmentViewModel
 import dagger.Binds
 import dagger.Module
 import dagger.multibindings.IntoMap
@@ -14,9 +13,4 @@ abstract class MainViewModelsModule {
     @IntoMap
     @ViewModelKey(MainActivityViewModel::class)
     abstract fun bindMainActivityViewModel(viewModel: MainActivityViewModel): ViewModel
-
-    @Binds
-    @IntoMap
-    @ViewModelKey(ProfileFragmentViewModel::class)
-    abstract fun bindProfileFragmentViewModel(viewModel: ProfileFragmentViewModel): ViewModel
 }
